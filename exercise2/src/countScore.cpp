af5da54bd7c9403b460283fcc0dde595cb97441c
JakubKedzierski <248915@student.pwr.edu.pl>
2021-06-02 19:28:26 +0200

    added second

--- exercise2/src/countScore.cpp
+++ exercise2/src/countScore.cpp
@@ -1,17 +1,25 @@
 #include "include/countScrabbleScore.hpp"
 #include <vector>
+#include <utility>
+	
 
 int countScrabbleScore(const std::string &word){
-    std::vector<int> values{1,2,3,4,5,8,10};
-    std::vector<std::vector<char>> characters{
-        {'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' },
-        {'D', 'G'},
-        {'B', 'C', 'M', 'P'},
-        {'F', 'H', 'V', 'W', 'Y' },
-        {'K'},
-        {'J', 'X'},
-        {'Q','Z'}
+
+    std::vector<std::pair<std::vector<char>,int>> charactersToValue{
+        {{'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' },1},
+        {{'D', 'G'}, 2},
+        {{'B', 'C', 'M', 'P'},3},
+        {{'F', 'H', 'V', 'W', 'Y' },4},
+        {{'K'},5},
+        {{'J', 'X'},8},
+        {{'Q','Z'},10}
      };
+
+    for(auto character:word){
+        for(auto pair:charactersToValue){
+            
+        }
+    }
 
 
     return 0;
